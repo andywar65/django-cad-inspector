452cfd3b84b69bbaeb77834f2c1b111c93e54504
andywar65 <andy.war1965@gmail.com>
2024-10-10 18:47:50 +0200

    static and media settings

--- project/project/settings.py
+++ project/project/settings.py
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = "media/"
+STATIC_ROOT = "/home/andywar65/django-cad-inspector/static"
+MEDIA_ROOT = "/home/andywar65/django-cad-inspector/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
