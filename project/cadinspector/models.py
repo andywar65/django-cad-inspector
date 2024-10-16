149ad8589011a9fc6cb0fd294dfe3ff8c4e5bf59
andywar65 <andy.war1965@gmail.com>
2024-10-16 19:07:09 +0200

    adding cad2hex function

--- project/cadinspector/models.py
+++ project/cadinspector/models.py
@@ -260,3 +260,15 @@ class Staging(models.Model):
                 else:
                     out += f"{nh3.clean(key)}: {nh3.clean(value)}\n"
             return out
+
+
+"""
+    Collection of utilities
+"""
+
+
+def cad2hex(color):
+    if isinstance(color, tuple):
+        return "#{:02x}{:02x}{:02x}".format(color[0], color[1], color[2])
+    rgb24 = colors.DXF_DEFAULT_COLORS[color]  # noqa
+    return "#{:06X}".format(rgb24)
