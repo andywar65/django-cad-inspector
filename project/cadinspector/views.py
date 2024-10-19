feeb74d42fdf8c25fbeba02e8d3420b8bf7bd8b8
andywar65 <andy.war1965@gmail.com>
2024-10-19 12:58:52 +0200

    adding entity and scene views

--- project/cadinspector/views.py
+++ project/cadinspector/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render  # noqa
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+from .models import Entity, Scene
+
+
+class EntityListView(ListView):
+    model = Entity
+    template_name = "cadinspector/entity_list.html"
+
+
+class EntityDetailView(DetailView):
+    model = Entity
+    template_name = "cadinspector/entity_detail.html"
+
+
+class SceneListView(ListView):
+    model = Scene
+    template_name = "cadinspector/scene_list.html"
+
+
+class SceneDetailView(DetailView):
+    model = Scene
+    template_name = "cadinspector/scene_detail.html"
