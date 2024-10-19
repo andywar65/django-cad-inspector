4bb8a0def4e071d2a461067b4fbd45d60224013c
andywar65 <andy.war1965@gmail.com>
2024-10-19 15:00:35 +0200

    redirect to scene list

--- project/project/urls.py
+++ project/project/urls.py
@@ -19,9 +19,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(pattern_name="cadinspector:scene_list")),
     path("3d/", include("cadinspector.urls", namespace="cadinspector")),
 ]
 
