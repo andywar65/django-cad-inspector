dacd642c639e2cccaf6f3a204e86386219dfcf3f
andywar65 <andy.war1965@gmail.com>
2024-12-12 20:23:56 +0100

    added wireframe to scene detail

--- src/cadinspector/models.py
+++ src/cadinspector/models.py
@@ -388,19 +388,19 @@ class Staging(models.Model):
     position = models.CharField(
         _("Position"),
         default="0 0 0",
-        max_length=50,
+        max_length=100,
         help_text="Left/Right - Up/Down - In/Out",
     )
     rotation = models.CharField(
         _("Rotation"),
         default="0 0 0",
-        max_length=50,
+        max_length=100,
         help_text="Pitch - Yaw - Roll",
     )
     scale = models.CharField(
         _("Scale"),
         default="1 1 1",
-        max_length=50,
+        max_length=100,
         help_text="Width - Heigth - Depth",
     )
     color = ColorField(_("Color"), default="#FFFFFF")
@@ -410,7 +410,7 @@ class Staging(models.Model):
         blank=True,
     )
     wireframe = models.BooleanField(
-        _("Data sheet"),
+        _("Wireframe"),
         default=False,
     )
 
